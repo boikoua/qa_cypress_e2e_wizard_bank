49885abc13b100135bc4769bff2bb78221a30f89
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-15 11:04:27 +0300

    solution

---
+++ cypress/e2e/hermioneAccount.spec.cy.js
@@ -0,0 +1,78 @@
+/// <reference types='cypress' />
+
+describe('Bank app', () => {
+  const user = 'Hermoine Granger';
+  const accountNumber = 1001;
+  const balance = 5096;
+  const deposit = 100;
+  const withdrawn = 1000;
+
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should provide the ability to work with Hermion bank account', () => {
+    cy.contains('.btn', 'Customer Login').click();
+    cy.get('#userSelect').select(user);
+    cy.contains('.btn', 'Login').click();
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', accountNumber)
+      .should('be.visible');
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', balance)
+      .should('be.visible');
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', 'Dollar')
+      .should('be.visible');
+
+    cy.get('[ng-class="btnClass2"]').click();
+    cy.get('[placeholder="amount"]').type(deposit);
+    cy.contains('[type="submit"]', 'Deposit').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain.text', 'Deposit Successful')
+      .should('be.visible');
+
+    cy.contains('[ng-hide=noAccount]', 'Account Number')
+      .contains('strong', balance + deposit)
+      .should('be.visible');
+
+    cy.get('[ng-class="btnClass3"]').click();
+    cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
+    cy.get('[placeholder="amount"]').type(withdrawn);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+    cy.get('[ng-show="message"]')
+      .should('contain.text', 'Transaction successful')
+      .should('be.visible');
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', balance + deposit - withdrawn)
+      .should('be.visible');
+
+    cy.get('[ng-class="btnClass1"]').click();
+
+    cy.get('#start').type('2025-04-01T00:00');
+    cy.get('#start').should('have.value', '2025-04-01T00:00');
+
+    cy.get('#anchor0 > :nth-child(3)').should('contain.text', 'Credit');
+    cy.get('#anchor0 > :nth-child(2)').should('contain.text', deposit);
+
+    cy.get('#anchor1 > :nth-child(3)').should('contain.text', 'Debit');
+    cy.get('#anchor1 > :nth-child(2)').should('contain.text', withdrawn);
+
+    cy.get('[ng-click="back()"]').click();
+
+    cy.get('#accountSelect').select('number:1003');
+
+    cy.get('[ng-class="btnClass1"]').click();
+
+    cy.get('table').should('not.contain', 'tbody');
+
+    cy.get('[ng-show="logout"]').click();
+
+    cy.contains('label', 'Your Name :');
+  });
+});
